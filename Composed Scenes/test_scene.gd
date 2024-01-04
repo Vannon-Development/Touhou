fdb807cc6ffc3bf9f9bb665e5cdf9f5f3cc11e02
dglusak <dglusak@gmail.com>
2024-01-04 03:33:56 -0500

    Vertical Enemy Spawns
    Test scene now spawns vertical enemies.
    Collision/Death setup working.

--- Composed Scenes/test_scene.gd
+++ Composed Scenes/test_scene.gd
@@ -1,2 +1,18 @@
 extends Node2D
 
+const _spawn_time: float = 2.5
+
+var _timer: float
+
+func _ready():
+	randomize()
+	_timer = 0
+
+func _process(delta: float):
+	_timer += delta
+	if _timer >= _spawn_time: _spawn_enemy()
+
+func _spawn_enemy():
+	_timer = 0
+	var enemy := VerticalEnemy.spawn(Vector2(randf_range(1920 / 3.0, 1920 * 2 / 3.0), -30))
+	add_child(enemy)
--- Object Scenes/Enemies/vertical_enemy.gd
+++ Object Scenes/Enemies/vertical_enemy.gd
@@ -1,9 +1,19 @@
-extends EnemyBase
+class_name VerticalEnemy extends EnemyBase
 
 @export var _speed: float
 
-func _physics_process(delta):
+static var _spawn_object: PackedScene = load("res://Object Scenes/Enemies/vertical_enemy.tscn")
+
+func _physics_process(delta: float):
 	position += Vector2(0, _speed * delta)
 
 func _left_screen():
 	queue_free()
+
+func _death_sequence():
+	queue_free()
+
+static func spawn(pos: Vector2) -> EnemyBase:
+	var obj := _spawn_object.instantiate() as EnemyBase
+	obj.position = pos
+	return obj
